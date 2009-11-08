699b0101dd12a1433917815e4359b10f81e0dda4
Shyam <shyam@readybytes.in>
2009-11-08 07:20:37 +0000

    -- added uninstaller -- remove php-code sniffer errors for api.php and core.php

--- source/components/com_xipt/admin/uninstall.xipt.php
+++ source/components/com_xipt/admin/uninstall.xipt.php
@@ -1,19 +1,30 @@
 <?php
+// Check to ensure this file is within the rest of the framework
+defined('_JEXEC') or die();
+
+jimport( 'joomla.filesystem.folder' );
+jimport('joomla.filesystem.file');
+
+require_once dirname(JPATH_BASE).DS.'administrator'.DS.'components'.DS.'com_xipt'.DS.'jspt_functions.php';
 function com_uninstall()
 {
-	//drop_database();
+	uncopyHackedFiles();
 }
 
-function drop_database()
+function uncopyHackedFiles()
 {
-	$db	=& JFactory::getDBO();
-	$query ='DROP TABLE `#__xipt_profiletypes`';
-	$db->setQuery( $query );
-	$db->query();
-	
-	$query ='DROP TABLE `#__xipt_profilefields`';
-	$db->setQuery( $query );
-	$db->query();
-	
-	return true;
+	$filestoreplace = getJSPTFileList();
+   
+	if($filestoreplace) 
+	foreach($filestoreplace AS $sourceFile => $targetFile)
+	{
+		$targetFileBackup = $targetFile.'.jxibak';
+		// delete this file
+		// Only delete if you have backup copy
+		if(JFile::exists($targetFile) && JFile::exists($targetFileBackup))
+		{
+			JFile::delete($targetFile);
+			JFile::move($targetFileBackup,$targetFile) or J ;
+		}		
+	}
 }
