df1a4dc869bb6738ed12fceea104c0ec09f16502
Vikas <vikas@readybytes.in>
2010-11-29 11:18:47 +0000

    kit updated some sql file modified

--- test/test/unit/front/models/sql/XiptApplicationsModelTest/testGetPluginFromId.start.sql
+++ test/test/unit/front/models/sql/XiptApplicationsModelTest/testGetPluginFromId.start.sql
@@ -52,22 +52,3 @@ INSERT INTO `#__plugins` (`id`, `name`, `element`, `folder`, `access`, `ordering
 (50, 'JomSocial User List', 'testing002', 'community', 0, 0, 1, 0, 0, 0, '0000-00-00 00:00:00', ''),
 (51, 'JSPT Community Plugin', 'xipt_community', 'community', 0, 0, 0, 0, 0, 0, '0000-00-00 00:00:00', ''),
 (52, 'JSPT System Plugin', 'xipt_system', 'system', 0, 0, 0, 0, 0, 0, '0000-00-00 00:00:00', '');;
-
-TRUNCATE TABLE `#__xipt_applications` ;;
-
-INSERT INTO `#__xipt_applications` (`id`, `applicationid`, `profiletype`) VALUES
-(1, 44, 4),
-(2, 44, 3),
-(3, 44, 1),
-(4, 48, 1),
-(5, 45, 3),
-(6, 47, 2),
-(7, 46, 2),
-(8, 49, 2),
-(9, 49, 3),
-(10, 49, 4),
-(11, 50, 1),
-(12, 50, 3),
-(13, 50, 4);;
-
-
