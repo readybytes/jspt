526a9c39f366d96b0ab26b69203ceffa577f8ab0
Manish <manish@readybytes.in>
2011-02-16 10:25:15 +0000

    now install-test case run witout any wanring or error

--- test/system/joomlaFramework.php
+++ test/system/joomlaFramework.php
@@ -19,8 +19,7 @@ require_once JPATH_LIBRARIES.DS.'joomla'.DS.'import.php';
 require_once JPATH_BASE .DS.'includes'.DS.'framework.php';
 require_once JPATH_BASE .DS.'libraries'.DS.'joomla'.DS.'factory.php';
 // Initalize frontend framework
-$mainframe =& JFactory::getApplication('site');
-$mainframe->initialise();
+JFactory::getApplication('site');
 
 define('XIPT_TEST_MODE', true);
 
