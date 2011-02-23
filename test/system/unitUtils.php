a5355d60e680d1f5a9b919d454f47029c5d2fc1e
Sonilika <sonilika@readybytes.in>
2011-02-23 12:53:27 +0000

    Fix issue in updateJoomlaConfiguartion in unitutils file

--- test/system/unitUtils.php
+++ test/system/unitUtils.php
@@ -28,22 +28,27 @@ class XiUnitTestCase extends PHPUnit_Framework_TestCase
   
   function updateJoomlaConfig($filter)
   {
-	  	$config =& JFactory::getConfig();		
+  		$config =& JFactory::getConfig();		
   		foreach($filter as $key=>$value)
   			$config->setValue($key,$value);
   		
 		jimport('joomla.filesystem.file');
 		$fname = JPATH_CONFIGURATION.DS.'configuration.php';
-		
 		system("sudo chmod 777 $fname");
+			
+		$configString = '';
+		if(TEST_XIPT_JOOMLA_16){
+			$configString = $config->toString('PHP', array('class' => 'JConfig', 'closingtag' => false));
+		}elseif(TEST_XIPT_JOOMLA_15){
+			$configString  = $config->toString('PHP', 'config', array('class' => 'JConfig'));
+		}else {
+			assert(0);
+		}
 		
-  		if (!JFile::write($fname, 
-  				$config->toString('PHP', 'config', array('class' => 'JConfig')) )
-  		    ) 
+  		if(!JFile::write($fname,$configString)) 
 		{
 			echo JText::_('ERRORCONFIGFILE');
 		}
-  		
   }
   
   function changeJomSocialConfig($filters)
