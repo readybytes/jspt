de864b2094f0418486e030735b63fc15aad7cd8d
Sonilika <sonilika@readybytes.in>
2011-03-04 08:36:44 +0000

    Running of seltest case: com_xipt/admin/aclRuleTest

--- test/test/com_xipt/admin/aclRulesTest.php
+++ test/test/com_xipt/admin/aclRulesTest.php
@@ -18,7 +18,12 @@ class AclRulesTest extends XiSelTestCase
     $this->waitPageLoad();
       
 	// add Rule-1
-    $this->click("//td[@id='toolbar-new']/a");
+  	if(TEST_XIPT_JOOMLA_15){
+		$this->click("//td[@id='toolbar-new']/a");
+    }
+    if(TEST_XIPT_JOOMLA_16){
+    	$this->click("//li[@id='toolbar-new']/a/span");
+    }
     $this->waitPageLoad();
     
     $this->select("//select[@id='acl']", "value=addalbums");
@@ -27,7 +32,12 @@ class AclRulesTest extends XiSelTestCase
     $this->type("rulename", "Can not Add Album more than 5");
     $this->type("aclparamsaddalbums_limit", "5");
     $this->select("coreparams[core_profiletype]", "label=PROFILETYPE-1");
-    $this->click("//td[@id='toolbar-save']/a/span");
+    if(TEST_XIPT_JOOMLA_15){
+		$this->click("//td[@id='toolbar-save']/a/span");
+    }
+    if(TEST_XIPT_JOOMLA_16){
+    	$this->click("//li[@id='toolbar-save']/a/span");
+    }
     $this->waitPageLoad();
     $this->assertTrue($this->isTextPresent("Can not Add Album more than 5"));
     $this->_DBO->addTable('#__xipt_aclrules');
