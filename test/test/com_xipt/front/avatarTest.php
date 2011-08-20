a70f26f83e209d7a339538cba3d4ebb48d1b6d5c
Richa <richa@readybytes.in>
2011-08-20 06:03:29 +0000

    add image-watermark test

--- test/test/com_xipt/front/avatarTest.php
+++ test/test/com_xipt/front/avatarTest.php
@@ -18,7 +18,10 @@ class AvatarTest extends XiSelTestCase
   	//login as profiletype 1
   	 $this->frontLogin($user->name,$user->name);
   	 $newAvatar = 'test/test/com_xipt/front/images/avatar_4.jpg';
-  	 $newAvatarAU = 'test/test/com_xipt/front/images/avatar_4_AU.jpg';
+  	 if(TEST_XIPT_JOOMLA_15)
+  	   $newAvatarAU = 'test/test/com_xipt/front/images/avatar_4_AU.jpg';
+  	 else
+  	   $newAvatarAU = 'test/test/com_xipt/front/images/avatar_4_AU_1.7.jpg';
   	 
   	 $this->open(JOOMLA_LOCATION."/index.php?option=com_community&view=profile&task=uploadAvatar");
 	 $this->waitPageLoad();
@@ -35,25 +38,68 @@ class AvatarTest extends XiSelTestCase
      $this->verifyAvatar($newAvatarAU);
     
     //change profiletype 1 to pt 3
-  	$newAvatarAU = 'test/test/com_xipt/front/images/avatar_5_AU.jpg';
-  	$this->editProfiletype(3);  
+     if(TEST_XIPT_JOOMLA_15)
+  	    $newAvatarAU = 'test/test/com_xipt/front/images/avatar_5_AU.jpg';
+  	else 
+       	$newAvatarAU = 'test/test/com_xipt/front/images/avatar_5_AU_1.7.jpg';
+     $this->editProfiletype(3);  
   	//Test: Profile-Type change and watr-mrk remove 
 	$this->verifyAvatar($newAvatarAU);
 
 	//change profiletype 3 to pt 2
-  	$newAvatarAU = 'test/test/com_xipt/front/images/avatar_6_AU.jpg';
+    if(TEST_XIPT_JOOMLA_15)
+  	    $newAvatarAU = 'test/test/com_xipt/front/images/avatar_6_AU.jpg';
+  	else 
+       	$newAvatarAU = 'test/test/com_xipt/front/images/avatar_6_AU_1.7.jpg';
   	$this->editProfiletype(2); 
   	//Test Profile-Type Change and water-mark apply 
   	$this->verifyAvatar($newAvatarAU);
 
   	//change profiletype 2 to 1
-  	$newAvatarAU = 'test/test/com_xipt/front/images/avatar_4_AU.jpg';
+  	if(TEST_XIPT_JOOMLA_15)
+  	    $newAvatarAU = 'test/test/com_xipt/front/images/avatar_4_AU.jpg';
+  	else 
+       	$newAvatarAU = 'test/test/com_xipt/front/images/avatar_4_AU_1.7.jpg';
   	$this->editProfiletype(1);
   	//Test: Profile-type change and watr-mark position change 
   	$this->verifyAvatar($newAvatarAU);
-    
+  	
+  	
   	$this->frontLogout();
 	$this->postConditions();
+  }
+  
+  function testUpdateavatarwithWatermrk()
+  {
+  	$this->preConditions();
+	$user = JFactory::getUser(82); 
+  	//login as profiletype 1
+  	 $this->frontLogin($user->name,$user->name);
+  	 $newAvatar = 'test/test/com_xipt/front/images/avatar_4.jpg';
+  	 $newAvatarAU = 'test/test/com_xipt/front/images/avatar_4_AU.jpg';
+  	 
+  	 $this->open(JOOMLA_LOCATION."/index.php?option=com_community&view=profile&task=uploadAvatar");
+	 $this->waitPageLoad();
+	 $this->assertTrue($this->isTextPresent("Change profile picture"));
+
+	 //upload new avatar
+	 $this->type("file-upload", JPATH_ROOT.DS.$newAvatar);
+	 $this->click("file-upload-submit");
+	 $this->waitPageLoad();
+	 $this->click("link=Profile");
+	 $this->waitPageLoad();
+     //Check avatar uploaded and watr-mrk apply
+     // $this->verifyAvatar($newAvatarAU);
+
+	 $watermrksrc='test/test/com_xipt/admin/images/watermark_4.png';
+     $watrmrkdest='images/profiletype/watermark_4.png';
+     $newAvatarAU = 'test/test/com_xipt/front/images/avatar_img_AU.jpg';
+     Jfile::copy(JPATH_ROOT.DS.$watermrksrc,JPATH_ROOT.DS.$watrmrkdest);
+     $this->editProfiletype(4);
+     //Test: Profile-type change and watr-mark position change 
+  	 //$this->verifyAvatar($newAvatarAU);
+  	 $this->frontLogout();
+	 $this->postConditions();
   }
   
   function verifyAvatar($newAvatarAU)
@@ -108,4 +154,5 @@ class AvatarTest extends XiSelTestCase
   		JFile::delete(JPATH_ROOT.DS."images/profiletype/watermark_".$i."_thumb.png");
   		}
   }
+  
 }
