0ad6dbadf16c837fe9681fbaa700b65d9576d90f
Richa <richa@readybytes.in>
2011-09-20 06:59:55 +0000

    test #422 video acl rule when sef is enabled and test cases fixing

--- source/site/libraries/acl/accessvideo/accessvideo.php
+++ source/site/libraries/acl/accessvideo/accessvideo.php
@@ -11,7 +11,16 @@ class accessvideo extends XiptAclBase
 	function getResourceOwner($data)
 	{
 		$videoId = isset($data['videoid']) ? $data['videoid'] : '';
-		$videoId	= JRequest::getVar( 'videoid' , $videoId );
+		$videoId = JRequest::getVar( 'videoid' , $videoId );
+		$conf    = JFactory::getConfig();
+        // if sef is enabled then get actual video id
+		if($conf->getValue('config.sef', false) == true)
+		{
+			$vId     = explode(":", $videoId);
+			$videoId = $vId[0];
+			
+		}
+		
 		$video	    = CFactory::getModel('videos');
 		$videoData  = $video->getVideos(array('id'=>$videoId));
 		$creatorid	= $videoData[0]->creator;
