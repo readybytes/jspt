8b357f20ccb65a06bc5e6a1955a47cfffd003cea
Khushboo <khushboo@readybytes.in>
2013-01-28 10:20:08 +0000

    fixed addasfriends acl rule

--- source/site/libraries/acl/addasfriends/addasfriends.php
+++ source/site/libraries/acl/addasfriends/addasfriends.php
@@ -13,6 +13,40 @@ class addasfriends extends XiptAclBase
 		return $data['args'][0];	
 	}
 	
+	function isApplicableOnMaxFeature($resourceAccesser,$resourceOwner)
+	{	
+		$aclSelfPtype = $this->getACLAccesserProfileType();
+		$otherptype   = $this->getACLOwnerProfileType();
+		
+		$aclSelfPtype = is_array($aclSelfPtype) ? implode(',', $aclSelfPtype) : $aclSelfPtype;
+		$otherPtype   = is_array($otherPtype)	? implode(',', $otherPtype)   : $otherPtype;
+		
+		$count = $this->getFeatureCounts($resourceAccesser,$resourceOwner,$otherptype,$aclSelfPtype);
+		$paramName = get_class($this).'_limit';
+		$maxmimunCount = $this->aclparams->get($paramName,0);
+		if($count >= $maxmimunCount)
+			return true;
+			
+		return false;
+	}
+
+	function isApplicableOnMaxFeatureByPlan($resourceAccesser,$resourceOwner)
+	{	
+		$aclSelfPlan = $this->getACLAccesserPlan();
+		$otherPlan   = $this->getACLOwnerPlan();
+		
+		$aclSelfPlan = is_array($aclSelfPlan) ? implode(',', $aclSelfPlan) : $aclSelfPlan;
+		$otherPlan	 = is_array($otherPlan)	  ? implode(',', $otherPlan)   : $otherPlan;
+		
+		$count = $this->getFeatureCountsByPlan($resourceAccesser,$resourceOwner,$otherPlan,$aclSelfPlan);
+		$paramName = get_class($this).'_limit';
+		$maxmimunCount = $this->aclparams->get($paramName,0);
+		if($count >= $maxmimunCount)
+			return true;
+			
+		return false;
+	}
+	
 	function getFeatureCounts($resourceAccesser,$resourceOwner,$otherptype,$aclSelfPtype)
 	{
 		// XITODO : change this query into object
@@ -22,13 +56,30 @@ class addasfriends extends XiptAclBase
 				. 'ON a.connect_from=' . $db->Quote( $resourceAccesser ) . ' '
 				. 'AND a.connect_to=b.id '
 				. ' LEFT JOIN #__xipt_users as ptfrom ON a.`connect_to`=ptfrom.`userid`'
-				. ' AND ptfrom .`profiletype`=' . $db->Quote($aclSelfPtype)
+				. ' AND ptfrom .`profiletype` IN(' . $db->Quote($aclSelfPtype) .')'
 				. ' LEFT JOIN #__xipt_users as ptto ON a.`connect_to`=ptto.`userid`'
-				. ' AND ptto .`profiletype`=' . $db->Quote($otherptype);
+				. ' AND ptto .`profiletype` IN(' . $db->Quote($otherptype) . ')';
 		$db->setQuery( $query );
 		$count		= $db->loadResultArray();
 		return count($count);
 	}	
+	
+	function getFeatureCountsByPlan($resourceAccesser,$resourceOwner,$otherplan,$aclSelfPlan)
+	{
+		// XITODO : change this query into object
+		$db		= JFactory::getDBO();
+		$query	= 'SELECT DISTINCT(a.connect_to) AS id  FROM ' . $db->nameQuote('#__community_connection') . ' AS a '
+				. 'INNER JOIN ' . $db->nameQuote( '#__users' ) . ' AS b '
+				. 'ON a.connect_from=' . $db->Quote( $resourceAccesser ) . ' '
+				. 'AND a.connect_to=b.id '
+				. ' LEFT JOIN #__payplans_subscription as planfrom ON a.`connect_to`=planfrom.`user_id`'
+				. ' AND planfrom .`plan_id` IN(' . $db->Quote($aclSelfPlan) .')'
+				. ' LEFT JOIN #__payplans_subscription as planto ON a.`connect_to`=planto.`user_id`'
+				. ' AND planto .`plan_id` IN(' . $db->Quote($otherplan) . ')';
+		$db->setQuery( $query );
+		$count		= $db->loadResultArray();
+		return count($count);
+	}
 	
 	function checkAclApplicable(&$data)
 	{
