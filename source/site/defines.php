3328157b7b6a528663bae57cfcbd9b65202acba5
rbslvivek <vivek@readybytes.in>
2014-10-15 18:57:27 +0530

    chnage version to 4.0.16

--- source/site/defines.php
+++ source/site/defines.php
@@ -8,7 +8,7 @@ if(!defined('_JEXEC')) die('Restricted access');
 
 defined('DS') or define('DS', DIRECTORY_SEPARATOR);
 
-defined('XIPT_VERSION') or define('XIPT_VERSION','4.0.15');
+defined('XIPT_VERSION') or define('XIPT_VERSION','4.0.16');
 
 define('XIPT_NOT_DEFINED','XIPT_NOT_DEFINED');
 define('XIPT_NONE','XIPT_NONE');
